2b2e40c6f2d6324ea7f070c8d55f12bd5cf11c0c
Atte Honkonen <atte.honkonen@gmail.com>
2021-03-27 17:49:44 +0200

    basic structure

--- voldemor/src/App.js
+++ voldemor/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [loggedIn, setLoggedIn] = useState(false);
+const [pw, setPw] = useState('');
+const [cred, setCred] = useState('');
+
+const handlePwFormChange = event => {
+  setPw(event.target.value);
+}
+
+const handlePwSubmit = event => {
+  // lähetä Azure funktiolle
+  console.log('submit', pw)
+  setLoggedIn(true);
+  setPw('');
+  event.preventDefault();
+
+}
+
+const handleLogOut = event => {
+console.log('logged out');
+setLoggedIn(false);
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testi <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {loggedIn ? <button onClick={handleLogOut}>log out</button> : 
+        <form onSubmit={handlePwSubmit}>
+        <input type="password" value={pw} onChange={handlePwFormChange}></input>
+        <input type="submit" value="Submit"></input>
+        </form>}
+
+        {loggedIn && <p>tähän tulee sitten niitä kuvia sitten</p>}
+
+
     </div>
   );
 }
